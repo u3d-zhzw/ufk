3cfd2fcaaf2b5ada664015ee84eb21669cf12015
zijie_li <425880365@qq.com>
2018-04-24 21:05:33 +0800

    build.sh

--- server/engine/build.sh
+++ server/engine/build.sh
@@ -1,17 +1,26 @@
+#!bin/sh
+
 BASE_PATH=$(cd `dirname $0`; pwd)
 BUILD_PATH=${BASE_PATH}/build
 
-APPLICATION_OUTPUT_FILE=${BUILD_PATH}/Application/appSrv
+LIB_OUTPUT_PATH=${BUILD_PATH}/lib
+DEPLOY_PATH=${BASE_PATH}/Deploy
 
-
-# check require
+# require
 # cmake 2.6 or above
 # gcc 4.8 or above
 
 sudo rm -rf ${BUILD_PATH}
 mkdir ${BUILD_PATH}
+cd ${BUILD_PATH}
 cmake ..
 make -j 4
+
+# TODO: check directionary existed
+# TODO: copy list
+APPSRV_OUTPUT=${BUILD_PATH}/Application/appSrv
+cp -vf ${APPSRV_OUTPUT} ${DEPLOY_PATH}/servers/bin/
+#cd ${LIB_OUTPUT_PATH}/* ${DEPLOY_PATH}/lib
 
 
 
