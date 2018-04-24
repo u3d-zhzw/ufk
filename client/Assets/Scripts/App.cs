984eb1b86a325b16bcf05faa265f75b95cb754b8
zijie_li <425880365@qq.com>
2018-04-24 17:05:59 +0800

    app & logger

---
+++ client/Assets/Scripts/App.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class App
+{
+    /// <summary>
+    /// 日志logger
+    /// </summary>
+    public Logger logger = new Logger();
+
+
+    /// <summary>
+    /// 服务端时间，单位毫秒
+    /// </summary>
+    private ulong _mstime;
+    public ulong mstime
+    {
+        get
+        {
+            //TODO
+            return this._mstime;
+        }
+    }
+
+
+    public void Init()
+    {
+        // TODO: 
+    }
+
+    public void SetServerTime(ulong ms)
+    {
+        this._mstime = ms; 
+    }
+
+    public ulong GetServerTime()
+    {
+        return this._mstime;
+    }
+}
---
+++ client/Assets/Scripts/Logger.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+/// <summary>
+/// todo: 
+/// 1. 控制台日志
+/// 2. 文件日志
+/// </summary>
+public class Logger
+    : ILogger
+{
+
+    public ILogHandler logHandler { get { throw new NotImplementedException(); } set { throw new NotImplementedException(); } }
+    public bool logEnabled { get { throw new NotImplementedException(); }  set { throw new NotImplementedException(); } }
+    public LogType filterLogType { get { throw new NotImplementedException(); } set { throw new NotImplementedException(); } }
+
+    public bool IsLogTypeAllowed(LogType logType)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Log(LogType logType, object message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Log(LogType logType, object message, UnityEngine.Object context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Log(LogType logType, string tag, object message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Log(LogType logType, string tag, object message, UnityEngine.Object context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Log(object message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Log(string tag, object message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Log(string tag, object message, UnityEngine.Object context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void LogError(string tag, object message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void LogError(string tag, object message, UnityEngine.Object context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void LogException(Exception exception)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void LogException(Exception exception, UnityEngine.Object context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void LogFormat(LogType logType, string format, params object[] args)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void LogFormat(LogType logType, UnityEngine.Object context, string format, params object[] args)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void LogWarning(string tag, object message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void LogWarning(string tag, object message, UnityEngine.Object context)
+    {
+        throw new NotImplementedException();
+    }
+}
