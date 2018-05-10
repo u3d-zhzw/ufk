c64472167deccc716864222c1321daed54143dbf
zijie_li <425880365@qq.com>
2018-05-10 09:16:59 +0800

    use protocol-lite

--- server/proto/pb2cpp.sh
+++ server/proto/pb2cpp.sh
@@ -4,7 +4,7 @@ BASEPATH=$(cd `dirname $0`; pwd)
 
 PROTO_BIN=$BASEPATH/../tools/protoc-3.4.0-linux-x86_64/bin/protoc
 
-PROTO_SRC=./Proto
+PROTO_SRC=./pb
 CPP_SRC=./cpp
 
 $PROTO_BIN -I=$PROTO_SRC --cpp_out=$CPP_SRC $PROTO_SRC/Person.proto
